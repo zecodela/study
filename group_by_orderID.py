997510e427036969af9232325d74c85b9a18ee71
zecodela <jim.hh.lo@gmail.com>
2018-07-01 23:02:21 +0800

    friday in office

--- group_by_orderID.py
+++ group_by_orderID.py
@@ -3,40 +3,40 @@ import sys
 import pandas as pd 
 
 def GroupByOrderID():
+	print("GroupByOrderID")
 	df = pd.read_csv("trade_file.csv")
 	print(df.info())
-	#print (df.head())
-	#print (df.index)
-	#df.set_index('symbol')
-	#print (df.columns)
-	#print (df.describe())
 	grouped = df.groupby(['orderID'])	
 	print(grouped.sum())
 	return
 
 def GroupByOrderId():
+    print("GroupByOrderId")
     #Load from csv
     df = pd.read_csv("25_Trade_Information_20180626.csv")
     #Select relevant columns
-    selecteddf = df.loc[:,['CustomerInfo','StockCode','BuySell','TradePrice','TradeSize']]
+    selecteddf = df.loc[:,['CustomerInfo','StockCode','BuySell','AccountID','TradePrice','TradeSize']]
     #Add Consideration field
-    #df['Consideration'] = df
+    selecteddf['Consideration'] = selecteddf['TradeSize'] * df['TradePrice']
+    print(df.columns)
     #Group by fields (show in report)
     gpdf = selecteddf.groupby(['CustomerInfo','StockCode','BuySell'])
-    print(gpdf.sum())
+    #gpdf['AvgPx'] = gpdf['Consideration'] * gpdf['TradeSize']
+    gpdf.sum().to_csv('GroupbyOrderId.csv')
     return
 
 
 def GroupByOrderID2():
+	print("GroupByOrderID2")
 	ipl_data = {'orderID': ['a001','a001','a001','a002','a002'],
 				'tradePx': ['100.1','100.15','100.2','20.0','20.1'],
 				'tradeVol': ['10000','1000','100','2000','3000'],
 				"symbol": ['1.hk','1.hk','1.hk','2.hk','2.hk'],
 				"side": ['b','b','b','s','s']}
 	df = pd.DataFrame(ipl_data)
-	print (df)			
- 	print (df.groupby(['orderID','symbol','side']).groups)
- 	return
+	print (df)
+	print (df.groupby(['orderID','symbol','side']).groups)
+	return
 
 def run():
 	GroupByOrderId()
