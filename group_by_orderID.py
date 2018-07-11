80deeae71fb7c5fd036e183c9c22f868288cdaa1
jlo <jim.hh.lo@gmail.com>
2018-07-11 13:48:28 +0800

    after jacky demo

--- group_by_orderID.py
+++ group_by_orderID.py
@@ -3,34 +3,40 @@ import sys
 import pandas as pd 
 
 
-def GroupByOrderId():
-    print("GroupByOrderId")
+def GroupByOrderId(inputfile):
+    #print("GroupByOrderId")
     #Load from csv
-    df = pd.read_csv("25_Trade_Information_20180626.csv")
-    #Select relevant columns
+    df = pd.read_csv(inputfile)
+    print ("Select relevant columns")
     selecteddf = df.loc[:,['CustomerInfo','StockCode','BuySell','AccountID','TradePrice','TradeSize']]
-    #Add Consideration field
+    print("Add Consideration field")
     selecteddf['Consideration'] = selecteddf['TradeSize'] * selecteddf['TradePrice']
-    print(selecteddf.columns)
-    #Group by fields (show in report)
+    #print(selecteddf.columns)
+    print("Group by fields (show in report)")
     gpdf = selecteddf.groupby(['CustomerInfo','StockCode','BuySell'])
-    #Aggregate by sum and count
+    print("Aggregate by sum and count")
     agggpdf = gpdf.agg({'TradePrice':'count','TradeSize':'sum','Consideration':'sum'})    
-    #Rename
+    print("Rename")
     sumgpdf = agggpdf.rename(columns={'TradePrice':'TradeCount'})
-    print(sumgpdf.columns)
-    #Add new field AvgPX....
+    #print(sumgpdf.columns)
+    print("Add new field AvgPX")
     sumgpdf['AvgPx'] = sumgpdf['Consideration'] / sumgpdf['TradeSize']
-    print(sumgpdf.columns)
-    print("Write to csv")
-    sumgpdf.to_csv('20180626_GroupbyOrderId.csv',sep=',')
+    #print(sumgpdf.columns)
+    print("Write to csv...")
+    outfile = inputfile.split('.')[0] +'_OrderFile.csv'
+    print("output file: " + outfile)
+    sumgpdf.to_csv(outfile,sep=',')
     return
 
 
 
 
 def run():
-	GroupByOrderId()
-	print("this is the end...")
+    inputfile = sys.argv[1]
+    if inputfile != "":
+        print("Source file: " + inputfile)
+        GroupByOrderId(inputfile)
+    else:
+        print("No input file provided.")
 if __name__ == '__main__':
 	run()
