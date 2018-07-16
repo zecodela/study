0847d69c747db48781ac2ac284db31882714b53c
jlo <jim.hh.lo@gmail.com>
2018-07-16 18:19:06 +0800

    re

--- cityhunter/parser.py
+++ cityhunter/parser.py
@@ -1,42 +1,15 @@
 
-import sys
-import pandas as pd 
+import re
 
-
-def GroupByOrderId(inputfile):
-    #print("GroupByOrderId")
-    #Load from csv
-    df = pd.read_csv(inputfile)
-    print ("Select relevant columns")
-    selecteddf = df.loc[:,['CustomerInfo','StockCode','BuySell','AccountID','TradePrice','TradeSize']]
-    print("Add Consideration field")
-    selecteddf['Consideration'] = selecteddf['TradeSize'] * selecteddf['TradePrice']
-    #print(selecteddf.columns)
-    print("Group by fields (show in report)")
-    gpdf = selecteddf.groupby(['CustomerInfo','StockCode','BuySell'])
-    print("Aggregate by sum and count")
-    agggpdf = gpdf.agg({'TradePrice':'count','TradeSize':'sum','Consideration':'sum'})    
-    print("Rename")
-    sumgpdf = agggpdf.rename(columns={'TradePrice':'TradeCount'})
-    #print(sumgpdf.columns)
-    print("Add new field AvgPX")
-    sumgpdf['AvgPx'] = sumgpdf['Consideration'] / sumgpdf['TradeSize']
-    #print(sumgpdf.columns)
-    print("Write to csv...")
-    outfile = inputfile.split('.')[0] +'_OrderFile.csv'
-    print("output file: " + outfile)
-    sumgpdf.to_csv(outfile,sep=',')
-    return
-
-
-
+def Find(pat, text):
+    match = re.search(pat, text)
+    if match:        
+        print (match.group())
+    else:
+        print ("Not found")
 
 def run():
-    inputfile = sys.argv[1]
-    if inputfile != "":
-        print("Source file: " + inputfile)
-        GroupByOrderId(inputfile)
-    else:
-        print("No input file provided.")
+    Find("tel=.*\b$", "tel=60716030 name=jimlo")
+        
 if __name__ == '__main__':
 	run()
