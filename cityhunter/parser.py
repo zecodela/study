0ed7872ac9d3f0ae2e83ba0b8760a2e5fdbab0c2
jlo <jim.hh.lo@gmail.com>
2018-07-20 18:59:34 +0800

    parse.py with unicode

--- cityhunter/parser.py
+++ cityhunter/parser.py
@@ -1,15 +1,17 @@
 
 import re
 
-def Find(pat, text):
-    match = re.search(pat, text)
-    if match:        
-        print (match.group())
-    else:
-        print ("Not found")
-
 def run():
-    Find("tel=.*\b$", "tel=60716030 name=jimlo")
-        
+	f = open("output.txt", encoding = 'utf8')
+	l = f.readline()
+	print(l)
+	#(title=\")(.*)(\">)'
+	matchobj = re.match(r"(id=)", l)
+	if matchobj:
+		print(matchobj.group(0))
+	else:
+		print("not found")
+	f.close()
+
 if __name__ == '__main__':
 	run()
